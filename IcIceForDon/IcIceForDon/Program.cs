89e2fa79435dbbdc7e62cfe20c06c47dd39114e8
unknown <helinkaya104101@gmail.com>
2019-08-23 16:02:32 +0300

    Triangle with stars

---
+++ IcIceForDon/IcIceForDon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcIceForDon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kaç satırlık üçgen olsun?");
+            int yıldız = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= yıldız; i++)
+            {
+                for (int j = i; j <= yıldız; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
